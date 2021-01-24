160f0d8537139ec54e3143d2dfedcc4924b2235c
Naruto-sys <gavriil.belov@yandex.ru>
2021-01-24 18:07:39 +0300

    added db

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from data import db_session
 
 app = Flask(__name__)
 
@@ -10,4 +10,5 @@ def index():
 
 
 if __name__ == '__main__':
+    db_session.global_init("db/database.sqlite")
     app.run(port=8080, host='127.0.0.1')
